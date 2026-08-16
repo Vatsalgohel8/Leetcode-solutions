9a8bc7698b6ab42bf2150d3c88f65313fbaca011
Vatsalgohel8 <gohelvatsal18@gmail.com>
2026-08-16 23:46:28 +0530

    Time: 1 ms (27.83%), Space: 14.2 MB (21.28%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        unordered_set<int>s(nums.begin(),nums.end());
+        priority_queue<int>pq;
+        for(auto it:s){
+            pq.push(it);
+        }
+        if(pq.size()<3){
+            return pq.top();
+        }
+        pq.pop();
+        pq.pop();
+        return pq.top();
+    }
+};
